bf0ce1f6aff413c4ae7d9b49a3064500270b5b57
igor <igoratakhanov@gmail.com>
2019-04-01 11:52:24 -0400

    push to dev gh hooks test

--- new_jenkins_ec2.sh
+++ new_jenkins_ec2.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 sudo iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 8080
 
@@ -9,3 +11,7 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo adduser jenkins
 sudo usermod -a -G docker jenkins
+sudo chkconfig docker on
+
+sudo reboot
+
