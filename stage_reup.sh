4873c0cb45541584bed04acafbcd49963febfe29
igor <igoratakhanov@gmail.com>
2019-04-25 13:28:40 -0400

    added absolute pathways to deploy script to fix file not found

--- stage_reup.sh
+++ stage_reup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+
 # stop current prod process
-docker-compose --file docker-compose-prod.yml down --volumes
+docker-compose --file ~/go-ribbit/docker-compose-prod.yml down --volumes
 
 # update source
 git checkout stage
 git pull origin stage
 
 # start updated app
-docker-compose --file docker-compose-prod.yml up -V -d --build
+docker-compose --file ~/go-ribbit/docker-compose-prod.yml up -V -d --build
